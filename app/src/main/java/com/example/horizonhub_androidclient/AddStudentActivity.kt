80cbcd372840b2a694136f292aa7d1823e47e1c8
RNA <rna0@protonmail.com>
2024-01-26 17:23:17 +0200

    add button, layout and open uri

---
+++ app/src/main/java/com/example/horizonhub_androidclient/AddStudentActivity.kt
@@ -0,0 +1,18 @@
+package com.example.horizonhub_androidclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_student)
+    }
+
+
+//    private fun setupUI() {
+//
+//        nameTextField = findViewById(R.id.etAddStudentName)
+//        idTextField = findViewById(R.id.etAddStudentID)
+//    }
+}
--- app/src/main/java/com/example/horizonhub_androidclient/MainActivity.kt
+++ app/src/main/java/com/example/horizonhub_androidclient/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.horizonhub_androidclient
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val addStudentButton: Button = findViewById(R.id.btnAddStudent)
+
+        addStudentButton.setOnClickListener(::openYoutubeLink)
+    }
+    fun onAddStudentButtonClicked(view: View) {
+        val intent = Intent(this, AddStudentActivity::class.java)
+        startActivity(intent)
+    }
+    fun openYoutubeLink(view: View) {
+        val youtubeLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // replace with your YouTube link
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+        startActivity(intent)
     }
 }
