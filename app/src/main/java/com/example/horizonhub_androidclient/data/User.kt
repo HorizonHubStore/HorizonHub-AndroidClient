dd45202ca77ccc8e6849dca5c69dad3f73997c99
RNA <rna0@protonmail.com>
2024-01-27 21:37:30 +0200

    removed logic

--- app/src/main/java/com/example/horizonhub_androidclient/data/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.horizonhub_androidclient.data
-
-data class User(val fullName: String, val email: String, val imagePath: String = "") {
-    constructor() : this("", "", "")
-}
--- app/src/main/java/com/example/horizonhub_androidclient/utilities/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.horizonhub_androidclient.utilities
-
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?) : Resource<T>(data)
-    class Error<T>(message: String?) : Resource<T>(null, message)
-    class Loading<T> : Resource<T>()
-}
