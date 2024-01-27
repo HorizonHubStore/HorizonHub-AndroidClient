1693fb21a140cf584ea9f7df90108b8918008602
RNA <rna0@protonmail.com>
2024-01-27 14:38:17 +0200

    finished login register template

--- app/src/main/java/com/example/horizonhub_androidclient/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/horizonhub_androidclient/activities/LoginRegisterActivity.kt
@@ -12,17 +12,5 @@ class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
-        val addStudentButton: Button = findViewById(R.id.btnAddStudent)
-
-        addStudentButton.setOnClickListener(::openYoutubeLink)
-    }
-    fun onAddStudentButtonClicked(view: View) {
-        val intent = Intent(this, ShoppingActivity::class.java)
-        startActivity(intent)
-    }
-    fun openYoutubeLink(view: View) {
-        val youtubeLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // replace with your YouTube link
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
-        startActivity(intent)
     }
 }
