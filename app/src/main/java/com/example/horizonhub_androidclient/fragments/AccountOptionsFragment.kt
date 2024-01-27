f5796ef35239dae120cd79feb27879c34856f238
RNA <rna0@protonmail.com>
2024-01-27 11:44:58 +0200

    created 2 fragments for the app to open

---
+++ app/src/main/java/com/example/horizonhub_androidclient/fragments/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.horizonhub_androidclient.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.horizonhub_androidclient.R
+
+class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+}
---
+++ app/src/main/java/com/example/horizonhub_androidclient/fragments/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.horizonhub_androidclient.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.horizonhub_androidclient.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
