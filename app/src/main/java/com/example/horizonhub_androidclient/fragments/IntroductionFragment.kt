aa43d7ab1b8567be71e18d751b267dd13cc12eae
rna <rna0@protonmail.com>
2024-02-02 21:32:09 +0200

    make buttons default instead of androidx

--- app/src/main/java/com/example/horizonhub_androidclient/fragments/IntroductionFragment.kt
+++ app/src/main/java/com/example/horizonhub_androidclient/fragments/IntroductionFragment.kt
@@ -1,8 +1,5 @@
 package com.example.horizonhub_androidclient.fragments
 
-import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.horizonhub_androidclient.R
 
