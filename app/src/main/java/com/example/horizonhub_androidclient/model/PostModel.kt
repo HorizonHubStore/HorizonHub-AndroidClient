39bb24454f8d03422dbc128463d6cb3e4de5fdb3
MrGlam <elonglam@gmail.com>
2024-02-08 19:23:09 +0200

    Add post model + modelview to firebase

---
+++ app/src/main/java/com/example/horizonhub_androidclient/model/PostModel.kt
@@ -0,0 +1,9 @@
+package com.example.horizonhub_androidclient.model
+
+data class PostModel(
+    val creator: String = "",
+    val gameName: String = "",
+    val gameImage: String = "",
+    val description: String = "",
+    val price: Long = 0
+)
